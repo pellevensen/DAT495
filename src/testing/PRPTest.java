4301e24636e83a1f3cf343532ec14d39a100d092
evensen <pelle@evensen.org>
2022-05-05 04:03:06 +0200

    Implemented a reverse order iterator, inReverse().

--- src/testing/PRPTest.java
+++ src/testing/PRPTest.java
@@ -5,9 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.HashMap;
-import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.SplittableRandom;
 
@@ -21,54 +22,57 @@ import testing.PRP.Builder;
  *
  */
 class PRPTest {
-	private static void testPRP_singleRun(long size, long seed) {
-		PRP p = PRP.Builder.size(size).seed(seed).build();
-		BitSet seen = new BitSet();
-		Iterator<Long> pIt = p.iterator();
+	private static void testPRPSingleRun(final long size, final long seed) {
+		final var p = PRP.Builder.size(size).seed(seed).build();
+		final var seen = new BitSet();
+		final var pIt = p.iterator();
 
-		for (int i = 0; i < size; i++) {
+		for (var i = 0; i < size; i++) {
 			@SuppressWarnings("boxing")
-			long v = pIt.next();
+			final long v = pIt.next();
 			assertTrue(v >= 0);
 			assertTrue(v < size);
-			int iv = (int) v;
+			final var iv = (int) v;
 			assertFalse(seen.get(iv));
 			seen.set(iv);
 		}
+
 		assertFalse(pIt.hasNext());
+		assertEquals(size, seen.cardinality());
+		assertEquals(-1, seen.nextSetBit((int) size));
 	}
 
 	@SuppressWarnings("boxing")
-	private static void testPRP_multiRuns(long size, long seed, int runs) {
-		PRP p = PRP.Builder.size(size).seed(seed).runs(runs).build();
-		Map<Long, Integer> hist = new HashMap<>();
-		Iterator<Long> pIt = p.iterator();
+	private static void testPRPMultiRuns(final int size, final long seed, final int runs) {
+		final var p = PRP.Builder.size(size).seed(seed).runs(runs).build();
+		final Map<Long, Integer> hist = new HashMap<>();
+		final var pIt = p.iterator();
 
-		for (int i = 0; i < size * runs; i++) {
-			long v = pIt.next();
+		for (var i = 0; i < size * runs; i++) {
+			final long v = pIt.next();
 			assertTrue(v >= 0);
 			assertTrue(v < size);
 			hist.merge(v, 1, (prev, one) -> prev + one);
 		}
-		for (long i = 0; i < size; i++) {
+		for (var i = 0L; i < size; i++) {
 			assertEquals(runs, hist.get(i));
 		}
 		assertFalse(pIt.hasNext());
 	}
 
-	private static int grow(int x, double factor) {
+	private static int grow(final int x, final double factor) {
 		return Math.max(x + 1, (int) (x * factor));
 	}
 
 	@SuppressWarnings("static-method")
 	@Test
 	void testPRP() {
-		SplittableRandom rng = new SplittableRandom(1);
-		for (int size = 0; size < 20000; size = grow(size, 1.31)) {
-			for (int j = 0; j < 10; j++) {
-				testPRP_singleRun(size, rng.nextLong());
-				for(int runs = 1; runs < 3; runs++) {
-					testPRP_multiRuns(size, rng.nextLong(), runs);
+		final var rng = new SplittableRandom(1);
+		for (var size = 0; size < 20000; size = grow(size, 1.31)) {
+			for (var j = 0; j < 10; j++) {
+				testPRPSingleRun(size, rng.nextLong());
+				for (var runs = 2; runs < 4; runs++) {
+					testPRPMultiRuns(size, rng.nextLong(), runs);
 				}
 			}
 		}
@@ -80,7 +84,60 @@ class PRPTest {
 		assertThrows(IllegalArgumentException.class, () -> Builder.size(-1));
 		assertThrows(IllegalArgumentException.class, () -> Builder.size(1).runs(-1));
 		assertThrows(IllegalStateException.class, () -> Builder.size(1L << 31).runs(1L << 31).build());
-		PRP perm = Builder.size(1L << 30).runs(1L << 31).build();
+		final var perm = Builder.size(1L << 30).runs(1L << 31).build();
 		assertTrue(perm.iterator().hasNext());
 	}
+
+	@Test
+	void testInReverseSingleRun() {
+		for (var size = 0; size < 20000; size = grow(size, 1.31)) {
+			final Map<Long, Integer> seen = new HashMap<>();
+			final var permutation = PRP.Builder.size(size).build();
+			var i = 0;
+			for (final long v : permutation) {
+				seen.put(v, i);
+				i++;
+			}
+			for (final long v : permutation.inReverse()) {
+				i--;
+				assertEquals(i, seen.get(v));
+			}
+		}
+	}
+
+	@Test
+	void testInReverseMultiRuns() {
+		for (var size = 0; size < 20000; size = grow(size, 1.31)) {
+			final List<Map<Long, Integer>> seen = new ArrayList<>();
+			for (var runs = 2; runs < 10; runs++) {
+				for (var r = 0; r < runs; r++) {
+					seen.add(new HashMap<>());
+				}
+				final var permutation = PRP.Builder.size(size).runs(runs).build();
+				var i = 0;
+				for (final long v : permutation) {
+					int mapIdx;
+					for (mapIdx = 0; mapIdx < runs; mapIdx++) {
+						if (!seen.get(mapIdx).containsKey(v)) {
+							break;
+						}
+					}
+					seen.get(mapIdx).put(v, i);
+					i++;
+				}
+				for (final long v : permutation.inReverse()) {
+					int mapIdx;
+					for (mapIdx = runs - 1; mapIdx >= 0; mapIdx--) {
+						if (seen.get(mapIdx).containsKey(v)) {
+							break;
+						}
+					}
+					i--;
+					assertEquals(i, seen.get(mapIdx).get(v));
+					seen.get(mapIdx).remove(v);
+				}
+			}
+		}
+	}
+
 }
